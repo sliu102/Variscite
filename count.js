944229d9420148be5fe27456176330e0761498cb
Vincent Liu <vxiao2046@gmail.com>
2017-09-25 15:12:20 +0800

    a4

--- count.js
+++ count.js
@@ -1,6 +1,17 @@
+<<<<<<< Updated upstream
 function count(age) {
 	console.log(age);
 	console.log('month');
+=======
+<<<<<<< Updated upstream
+function count(cunt) {
+	console.log(cunt);
+=======
+function count(month) {
+	console.log(month);
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
 
 count(11);
+
