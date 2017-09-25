f5cd6ea2e77394f4aaddde5dc647b69b51e722ea
aliceshen227 <Alice_Shen1@epam.com>
2017-09-25 15:10:51 +0800

    b4

--- count.js
+++ count.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 function count(age) {
 	console.log(age);
 	console.log('month');
-=======
-function count(cunt) {
-	console.log(cunt);
->>>>>>> d910e6207b9ee9874e6cc78a31741021381ca20f
 }
 
 count(11);
